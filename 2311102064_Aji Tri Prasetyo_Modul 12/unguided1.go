84342793c04ded9ed4dfcc642b2511185fea0fd2
ajitriprasetyo <162905118+ajitriprasetyo@users.noreply.github.com>
2024-11-25 17:43:01 +0700

    modul 12

---
+++ 2311102064_Aji Tri Prasetyo_Modul 12/unguided1.go
@@ -0,0 +1,65 @@
+package main
+import "fmt"
+
+func selectionSortAsc(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		minIdx := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
+	}
+}
+
+func main() {
+	var n int
+	fmt.Println("Input")
+	fmt.Scan(&n)
+
+	if n <= 0 || n >= 1000 {
+		fmt.Println("Jumlah daerah harus di antara 1 dan 999.")
+		return
+	}
+	masukan := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		var m int
+		fmt.Scan(&m)
+
+		if m <= 0 || m >= 1000000 {
+			fmt.Println("Jumlah rumah harus di antara 1 dan 999999.")
+			return
+		}
+
+		masukan[i] = make([]int, m)
+		for j := 0; j < m; j++ {
+			fmt.Scan(&masukan[i][j])
+		}
+	}
+
+	fmt.Println("\nOutput:")
+	for _, daerah := range masukan {
+		var ganjil []int
+		var genap []int
+
+		for _, num := range daerah {
+			if num%2 == 0 {
+				genap = append(genap, num)
+			} else {
+				ganjil = append(ganjil, num)
+			}
+		}
+
+		selectionSortAsc(ganjil)
+		selectionSortAsc(genap)
+		hasil := append(ganjil, genap...)
+
+		for _, val := range hasil {
+			fmt.Print(val, " ")
+		}
+		fmt.Println()
+	}
+}
