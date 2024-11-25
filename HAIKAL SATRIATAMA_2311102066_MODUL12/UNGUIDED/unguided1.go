36f0467f2c61876ecd3ed98954f4aa5416cef176
Kaelll <163104333+Kkaell@users.noreply.github.com>
2024-11-26 00:05:04 +0700

    glory glory man united

---
+++ HAIKAL SATRIATAMA_2311102066_MODUL12/UNGUIDED/unguided1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var n int
+	fmt.Print("Masukkan jumlah daerah (n): ")
+	fmt.Scan(&n)
+
+	if n <= 0 || n >= 1000 {
+		fmt.Println("n harus lebih besar dari 0 dan kurang dari 1000.")
+		return
+	}
+
+	for i := 0; i < n; i++ {
+		var m int
+		fmt.Printf("Masukkan jumlah rumah kerabat untuk daerah ke-%d: ", i+1)
+		fmt.Scan(&m)
+
+		if m <= 0 || m >= 1000000 {
+			fmt.Println("m harus lebih besar dari 0 dan kurang dari 1000000.")
+			return
+		}
+
+		houses := make([]int, m)
+		fmt.Printf("Masukkan nomor rumah kerabat untuk daerah ke-%d: ", i+1)
+		for j := 0; j < m; j++ {
+			fmt.Scan(&houses[j])
+		}
+
+		var odd, even []int
+		for _, house := range houses {
+			if house%2 == 0 {
+				even = append(even, house)
+			} else {
+				odd = append(odd, house)
+			}
+		}
+
+		sort.Ints(odd)
+		sort.Ints(even)
+
+		fmt.Printf("Hasil urutan rumah untuk daerah ke-%d: ", i+1)
+		for _, house := range odd {
+			fmt.Printf("%d ", house)
+		}
+		for _, house := range even {
+			fmt.Printf("%d ", house)
+		}
+		fmt.Println()
+	}
+}
---
+++ HAIKAL SATRIATAMA_2311102066_MODUL12/UNGUIDED/unguided2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func hitungMedian(data []int) int {
+	n := len(data)
+	if n == 0 {
+		return 0
+	}
+	if n%2 != 0 {
+		return data[n/2]
+	}
+	return (data[n/2-1] + data[n/2]) / 2
+}
+
+func main() {
+	var masukan []int
+	var data []int
+
+	for {
+		var num int
+		fmt.Scan(&num)
+
+		if num < 0 {
+			break
+		}
+
+		if num == 0 {
+
+			tempData := make([]int, len(data))
+			copy(tempData, data)
+			sort.Ints(tempData)
+			median := hitungMedian(tempData)
+			masukan = append(masukan, median)
+		} else {
+
+			data = append(data, num)
+		}
+	}
+
+	fmt.Println("Hasil keluaran:")
+	for _, median := range masukan {
+		fmt.Println(median)
+	}
+}
